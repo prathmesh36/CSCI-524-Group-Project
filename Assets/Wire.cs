f61b288dc522500ab6bb8ee3e37a53b057113ab9
suchethg <suchethg@isi.edu>
2023-10-16 21:38:25 -0700

    sucheth-wiring-game

---
+++ Assets/Wire.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wire : MonoBehaviour
+{
+    public SpriteRenderer wireEnd;
+    public GameObject lightOn;
+    Vector3 startPoint;
+    Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPoint = transform.parent.position;
+        startPosition = transform.position;
+    }
+
+    private void OnMouseDrag()
+    {
+        // mouse position to world point
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        newPosition.z = 0;
+
+        // check for nearby connection points
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(newPosition, .2f);
+        foreach (Collider2D collider in colliders)
+        {
+            if (collider.gameObject != gameObject)
+            {
+                // update wire to the connection point position
+                UpdateWire(collider.transform.position);
+
+                Wire wireComponent = collider.GetComponent<Wire>();
+                if (wireComponent != null)
+                {
+                    // check if the wires are the same color
+                    if (transform.parent.name.Equals(collider.transform.parent.name))
+                    {
+                        // count connection
+                        Main.Instance.SwitchChange(1);
+
+                        wireComponent.Done();
+                    }
+                }
+                Done();
+                return;
+            }
+        }
+
+        // update wire
+        UpdateWire(newPosition);
+    }
+
+    void Done()
+    {
+        // turn on light
+        lightOn.SetActive(true);
+
+        // destroy the script
+        Destroy(this);
+    }
+
+    private void OnMouseUp()
+    {
+        // reset wire position
+        UpdateWire(startPosition);
+    }
+
+    void UpdateWire(Vector3 newPosition)
+    {
+        // update position
+        transform.position = newPosition;
+
+        // update direction
+        Vector3 direction = newPosition - startPoint;
+        transform.right = direction * transform.lossyScale.x;
+
+        // update scale
+        float dist = Vector2.Distance(startPoint, newPosition);
+        wireEnd.size = new Vector2(dist, wireEnd.size.y);
+    }
+}
---
+++ Assets/Wire_Task_Art/Wiring.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wiring : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public SpriteRenderer wireEnd;
+    public GameObject lightOn;
+    Vector3 startPoint;
+    Vector3 startPosition;
+    void Start()
+    {
+        startPoint = transform.parent.position;
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    private void OnMouseDrag()
+    {
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        newPosition.z = 0;
+        //check for nearby connection points
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(newPosition, .2f);
+        foreach(Collider2D collider in colliders)
+        {
+            //make sure not my own collider
+            if (collider.gameObject != gameObject)
+            {
+                //update wire to the connection point position
+                UpdateWire(collider.transform.position);
+                if (transform.parent.name.Equals(collider.transform.parent.name))
+                {
+                    //count connection
+                    Main.Instance.SwitchChange(1);
+                    //finish step
+                    collider.GetComponent<Wiring>()?.Done();
+                    Done();
+                }
+                return;
+            }
+        }
+        UpdateWire(newPosition);
+
+    }
+    void Done()
+    {
+        //turn on lights
+        lightOn.SetActive(true);
+        //destroy the script
+        Destroy(this);
+    }
+    private void OnMouseUp()
+    {
+        UpdateWire(startPosition);
+
+    }
+    void UpdateWire(Vector3 newPosition)
+    {
+        transform.position = newPosition;
+        Vector3 direction = newPosition - startPoint;
+        transform.right = direction * transform.lossyScale.x;
+
+        float dist = Vector2.Distance(startPoint, newPosition);
+        wireEnd.size = new Vector2(dist, wireEnd.size.y);
+    }
+}
