c1906547498253bd31aa733d63403e3679b97816
DhanakTwinkle <dhanak@usc.edu>
2023-10-17 12:14:31 -0700

    Adding extra images for Menu UI screen
    Adding extra images for Menu UI screen

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,6 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
     public void PlayButton(){
+        Debug.Log("Inside PlayButton!");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
