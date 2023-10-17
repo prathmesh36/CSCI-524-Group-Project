0f1f6dce06fdaac8c3f8f74c492b99a612d57590
DhanakTwinkle <dhanak@usc.edu>
2023-10-16 21:11:22 -0700

    Menu UI Screen with Levels, Game Over and Game Settings
    Menu UI Screen with Levels, Game Over and Game Settings

---
+++ Assets/Scripts/GameControls.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameControls : MonoBehaviour
+{
+    public void PauseButton()
+    {
+        // Pause the game by setting the time scale to 0
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        // Unpause the game by setting the time scale back to 1
+        Time.timeScale = 1f;
+    }
+
+    public void QuitGame()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+    
+}
+
+
+
