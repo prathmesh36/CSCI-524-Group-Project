c0965fbbc4e4927b4ec63f09c6b0edb9f758ad95
prathmesh36 <prathmesh1297@outlook.com>
2023-10-17 23:32:35 -0700

    Analytics Added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static long startTime = 0;
+    public static long endTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -2,10 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
 
 public class GameOver : MonoBehaviour
 {
     public void StartOver(){
+        DateTime currentTime = DateTime.Now;
+        long unixTimestamp = (long)(currentTime - new DateTime(1970, 1, 1)).TotalSeconds;
+        GameManager.endTime = unixTimestamp;
+
+        Analytics analytics = new Analytics();
+        OverallGameAnalytics overallGameAnalytics = new OverallGameAnalytics();
+        overallGameAnalytics.timeTakenToFinishGame = (int)(GameManager.endTime - GameManager.startTime);
+        string jsonOverallGameAnalytics = JsonUtility.ToJson(overallGameAnalytics);
+        analytics.SaveData("overall-game-data.json", jsonOverallGameAnalytics);
         SceneManager.LoadScene("MainMenu");
     }
 }
