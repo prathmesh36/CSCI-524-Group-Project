2003d6fc5873f721fee12baa02f4f76f3510bdfb
DhanakTwinkle <dhanak@usc.edu>
2023-10-27 11:29:04 -0700

    Resolving merge conflict; Minor bug fix for Level Menu
    Resolving merge conflict; Minor bug fix for Level Menu

--- Assets/Scripts/LevelMenu.cs
+++ Assets/Scripts/LevelMenu.cs
@@ -11,7 +11,8 @@ public class LevelMenu : MonoBehaviour
         DateTime currentTime = DateTime.Now;
         long unixTimestamp = (long)(currentTime - new DateTime(1970, 1, 1)).TotalSeconds;
         GameManager.startTime = unixTimestamp;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("MyGame");
     }
 
      public void LoadTutorial(){
