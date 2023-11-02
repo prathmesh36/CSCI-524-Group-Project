04e85cab3dff96ada43f80dedcfdd8ad96772950
DhanakTwinkle <dhanak@usc.edu>
2023-11-01 21:58:24 -0700

    Analytics for users' Checkpoint behaviour
    Analytics for users' Checkpoint behaviour

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -1,5 +1,12 @@
 public class Constants
 {
+
+    // First checkpoint behaviour - Landing on Planet/Blackhole/Infinite Space
+    public const string TOTAL_SPACEBAR_CLICKS = "TotalSpacebarClicks";
+    public const string COUNT_INFINITY = "CountInfinity";
+    public const string COUNT_BLACKHOLE = "CountBlackhole";
+    public const string COUNT_PLANETS = "CountPlanets";
+
     // First Mini Game - Space Monsters
     public const string MONSTERS_KILLED = "MonstersKilled";
     public const string TOTAL_MONSTERS = "TotalMonsters";
@@ -7,5 +14,7 @@ public class Constants
     // Second Mini Game - Pipe Puzzle
     public const string TOTAL_MOUSE_CLICKS = "TotalMouseClicks";
     public const string TOTAL_MINES_CLICKS = "TotalMineClicks";
+
+
 
 }
--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -9,9 +9,8 @@ public class GameOver : MonoBehaviour
         DateTime currentTime = DateTime.Now;
         long unixTimestamp = (long)(currentTime - new DateTime(1970, 1, 1)).TotalSeconds;
         GameManager.endTime = unixTimestamp;
-        Debug.Log("GamerOver Start Script called and Game Scores Initialized");
-        GameManager.fuel = 45;
-        GameManager.health = 40;
+        GameManager.fuel = 40;
+        GameManager.health = 100;
         GameManager.boolArray = new bool[10];
         GameManager.currentPlanet = 0;
         GameManager.initialLoad = true;
@@ -34,33 +33,15 @@ public class GameOver : MonoBehaviour
         checkpointAnalytics.countSpacebarClicks = checkpointAnalytics.countBlackhole + checkpointAnalytics.countInfinity
             + checkpointAnalytics.countPlanets;
 
-        string jsonCheckpoint = JsonUtility.ToJson(checkpointAnalytics);
-        Analytics.Instance.SaveData("checkpoint-data.json", jsonCheckpoint);    
 
+        string json = JsonUtility.ToJson(checkpointAnalytics);
+        Analytics.Instance.SaveData("checkpoint-data.json", json);
 
-        // Analytics for overall fuel value trend over time
-        FuelAnalytics fuelAnalytics = new FuelAnalytics();
-        fuelAnalytics.blackholeImpact = PlayerPrefs.GetInt(Constants.BLACKHOLE_IMPACT);
-        fuelAnalytics.infiniteSpaceImpact = PlayerPrefs.GetInt(Constants.INFINITE_SPACE_IMPACT);
-        fuelAnalytics.asteroidImpact = PlayerPrefs.GetInt(Constants.ASTEROID_IMPACT); // @TODO: Yet to set Asteroid impact
-        fuelAnalytics.planetImpact = PlayerPrefs.GetInt(Constants.PLANET_IMPACT);
-        
-        string jsonFuelAnalytics = JsonUtility.ToJson(fuelAnalytics);
-        Analytics.Instance.SaveData("fuelAnalytics-data.json", jsonFuelAnalytics);  
-
-        // Analytics #1 - Reset PlayerPrefs again
+        // Reset PlayerPrefs again
         PlayerPrefs.SetInt(Constants.TOTAL_SPACEBAR_CLICKS, 0);
         PlayerPrefs.SetInt(Constants.COUNT_INFINITY, 0);
         PlayerPrefs.SetInt(Constants.COUNT_BLACKHOLE, 0);
         PlayerPrefs.SetInt(Constants.COUNT_PLANETS, 0);
-
-        // Analytics #2 - Reset
-        PlayerPrefs.SetInt(Constants.TOTAL_FUEL, 100);
-        PlayerPrefs.SetInt(Constants.BLACKHOLE_IMPACT , 0); 
-        PlayerPrefs.SetInt(Constants.ASTEROID_IMPACT , 0); 
-        PlayerPrefs.SetInt(Constants.PLANET_IMPACT , 0); 
-        PlayerPrefs.SetInt(Constants.INFINITE_SPACE_IMPACT , 0); 
-
 
     }
 
