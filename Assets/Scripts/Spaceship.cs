723dbf5444fab7a99b9d1e7120dab151ba4b5cb8
Unnati Babruwad <40277323+babruwadunnati@users.noreply.github.com>
2023-11-02 21:16:12 -0700

    Revert "Revert "Puzzle Game 1 connection""
    This reverts commit af199eb0e28256c08cac9891f1c5dd14cabbf2ce.

--- Assets/Scripts/Spaceship.cs
+++ Assets/Scripts/Spaceship.cs
@@ -23,9 +23,15 @@ public class Spaceship : MonoBehaviour
     public void TakeDamage()
     {
         health = health - 5;
+        
+
         if (health <= 0)
         {
+            
             // To do : Lost the mini game so something has to be shown here
+            PlayerPrefs.SetInt("SpaceGerms", -1);
+            
+
             SceneManager.LoadScene("MyGame");
         }
         healthDisplay.text = "Health: " + health;
@@ -37,7 +43,6 @@ public class Spaceship : MonoBehaviour
         if (health >= 100)
         {
             //To Do: Won the mini game. So health has to be increased
-            //SceneManager.LoadScene("YouWonMiniSpaceGermsGame");
 
             //Unnati: Who set the wrong key for player prefs? Wrong key was set for Playerprefs and this error took me 2 hours to understand
             PlayerPrefs.SetInt("SpaceGerms", 1);
