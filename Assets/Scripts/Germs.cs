59cb5c2c55ca23a982c2b16429a08bde9f4e5263
sucheth19 <130819674+sucheth19@users.noreply.github.com>
2023-11-23 15:17:32 -0800

    black hole

--- Assets/Scripts/Germs.cs
+++ Assets/Scripts/Germs.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class Germs : MonoBehaviour
 {
+    public GameObject explosion;
     public float speed;
     Spaceship spaceship;
     // Start is called before the first frame update
@@ -34,7 +35,19 @@ public class Germs : MonoBehaviour
             spaceship.TakeDamage();
             speed = 0;
             transform.parent = spaceship.transform;
+                DestroyWithExplosion(other.gameObject);
 
+        }
+        void DestroyWithExplosion(GameObject target)
+        {
+            GameObject expl = Instantiate(explosion, target.transform.position, Quaternion.identity) as GameObject;
+            if (target.tag == "Ketanu")
+            {
+                Destroy(target); // Destroy the Ketanu
+            }
+          // Destroy the Ketanu
+            Destroy(gameObject); // Destroy the Germs GameObject
+            Destroy(expl, 3); // Destroy the explosion after 3 seconds
         }
     }
 }
